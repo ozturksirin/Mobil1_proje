0fc97246daf5e60094ae188b2bc59c8e167b14de
ozturk sirin <ozturk2002.zri@gmail.com>
2022-04-14 18:21:49 +0300

    bugs fixed

--- app/src/main/java/com/example/ustamyanimda/Screen1.java
+++ app/src/main/java/com/example/ustamyanimda/Screen1.java
@@ -2,16 +2,13 @@ package com.example.ustamyanimda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-class Screen1 extends AppCompatActivity {
+public class Screen1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
-
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionBar))); //actionbar rengini değiştiriyorum
     }
 }
