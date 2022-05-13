5c388c0b1731e9d4dc512086a71fa0cac3b1f023
ozturk sirin <ozturk2002.zri@gmail.com>
2022-05-13 20:05:45 +0300

    screen1_menuler spinner_list add

--- app/src/main/java/com/example/ustamyanimda/Screen1_menuler.java
+++ app/src/main/java/com/example/ustamyanimda/Screen1_menuler.java
@@ -6,11 +6,21 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Screen1_menuler extends AppCompatActivity {
     private Button btn_ustaları_goster_go;
     private Button btn_usta_kayit_go;
+    private Spinner spinner_otomotiv;
+    private ArrayList<String> bolumler=new ArrayList<>();
+    private ArrayAdapter<String> veriAdaptoru;
+
 
 
     @Override
@@ -42,7 +52,41 @@ public class Screen1_menuler extends AppCompatActivity {
             }
         });
 
+        spinner_otomotiv=findViewById(R.id.spinner_bolumler);
 
+        bolumler.add(">OTOMOTİV");
+        bolumler.add(">BOYA");
+        bolumler.add(">DÖŞEME");
+        bolumler.add(">EKSPERTİZ");
+        bolumler.add(">ELEKTRİK");
+        bolumler.add(">KAPORTA");
+        bolumler.add(">KARAVAN");
+        bolumler.add(">KURTARMA/ÇEKİCİ");
+        bolumler.add(">LASTİK");
+        bolumler.add(">MEKANİK");
+        bolumler.add(">MOTOR");
+        bolumler.add(">OTO KİRALAMA");
+        bolumler.add(">RADYATÖR/KLİMA");
+        bolumler.add(">SERVİS");
+        bolumler.add(">TEMİZLİK");
+
+        veriAdaptoru=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,android.R.id.text1,bolumler);
+
+
+        spinner_otomotiv.setAdapter(veriAdaptoru);
+
+        spinner_otomotiv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"Seçilen Ustalar : "+bolumler.get(i),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
     }
